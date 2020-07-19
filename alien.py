1c49fcf27dd3e6dbc3195c736b2863b478e07ef1
MarioG8 <golikmariusz7@gmail.com>
2020-07-19 22:24:51 +0200

    Modify and add files

--- alien.py
+++ alien.py
@@ -8,14 +8,21 @@ class Alien(Sprite):
         """Initialize the alien and define its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
-        #Read the alien image and define its rect attribute.
+        # Read the alien image and define its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
-        #Place a new alien near the top left corner of the screen.
+        # Place a new alien near the top left corner of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        #Storing the exact horizontal alien position.?!
+        # Storing the exact horizontal alien position.?!
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right or left."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
+        
--- settings.py
+++ settings.py
@@ -3,17 +3,23 @@ class Settings:
 
     def __init__(self):
         """Initializing game settings."""
-        #Screen settings.
+        # Screen settings.
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
-        #Spacecraft settings speed.
+        # Spacecraft settings speed.
         self.ship_speed = 1.5
         
-        #Bullets settings.
+        # Bullets settings.
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien related settings.
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # A fleet_direction value of 1 is right and -1 is left.
+        self.fleet_direction = 1
