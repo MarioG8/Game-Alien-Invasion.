80c2739fa0cae117c55e2ee6b7ce5eb1cc48fa70
MarioG8 <golikmariusz7@gmail.com>
2020-07-21 15:32:01 +0200

    Modify file

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,9 +36,13 @@ class AlienInvasion:
         """Start the main game loop."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self._check_events()
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
 
@@ -115,22 +119,37 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Search for aliens reaching the bottom of the screen.
+        self._check_aliens_bottom()
+
+
+    def _check_aliens_bottom(self):
+        """Checks if any aliens reached the bottom of the screen."""      
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Tak samo jak w przypadku zderzenia statku z obcym.
+                self._ship_hit()
+                break
 
     def _ship_hit(self):
         """Alien hitting a ship."""
+        if self.stats.ships_left > 0:
         # Zmniejszenie wartości przechowywanej w ships_left.
-        self.stats.ships_left -= 1
+            self.stats.ships_left -= 1
 
-        # Removal of aliens and bullets lists.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Removal of aliens and bullets lists.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause ..
-        sleep(0.5)
+            # Pause ..
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
 
     
