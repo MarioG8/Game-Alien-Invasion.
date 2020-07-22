30928a23e4954163f74d2581b93bc82879119e60
MarioG8 <golikmariusz7@gmail.com>
2020-07-22 11:20:24 +0200

    modify current files

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,6 +59,30 @@ class AlienInvasion:
                     self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self._check_play_button(mouse_pos)
+
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the user clicks the Game button."""
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset game settings.
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            # Removal of aliens and bullets lists.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
 
     def _check_keydown_events(self, event):
         """Reaction to pressing a key."""
@@ -154,6 +178,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
 
     
